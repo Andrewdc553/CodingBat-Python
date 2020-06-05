5964a76a237336daf317a54b7b8cf3b70624aee0
Andrewc <62458351+Andrewdc553@users.noreply.github.com>
2020-06-05 22:14:29 +1200

    Update Logic_2.py

--- Logic_2.py
+++ Logic_2.py
@@ -43,4 +43,15 @@ and values to its right do not count. So for example, if b is 13, then both b an
     sum += n
   return sum  
 
+def no_teen_sum(a, b, c):
+    """
+    Given 3 int values, a b c, return their sum. However, 
+    if any of the values is a teen -- in the range 13..19 inclusive -- then that value counts as 0,
+    except 15 and 16 do not count as a teens. Write a separate helper "def fix_teen(n):"that takes in 
+    an int value and returns that value fixed for the teen rule. In this way, you avoid repeating the 
+    teen code 3 times (i.e. "decomposition"). Define the helper below and at the same indent level as the main no_teen_sum().
+    """
+  return fix_teen(a) + fix_teen(b) + fix_teen(c)
   
+def fix_teen(n):
+  return n if n not in [13, 14, 17, 18, 19] else 0
